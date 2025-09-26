b8a02209aa8b0b9beaf67da9f0cfc6d20bc8daab
Anmol Rastogi <ani16rs@gmail.com>
2025-09-26 10:58:57 -0700

    comments

--- src/biometric_auth_pydantic_ai/router.py
+++ src/biometric_auth_pydantic_ai/router.py
@@ -14,7 +14,7 @@ router_agent = Agent(
     output_type=ModalityChoice,
     system_prompt=(
         "You are a biometric authentication router. "
-        "Given a user request, decide the authentication modality. Use the choose_modality function for this."
+        "Given a user request, decide the authentication modality."
         "Valid options: voice, face, fingerprint, iris, password. "
         "Always respond with {modality: string}."
     ),
@@ -23,4 +23,4 @@ router_agent = Agent(
 def choose_modality(instruction: str) -> ModalityChoice:
     """Run the router agent and return the chosen modality"""
     # using run_sync so that nothing else runs while LLM decides modality
-    return router_agent.run_sync(instruction)       # todo: change run_sync to run if 
+    return router_agent.run_sync(instruction)       # todo: change run_sync to run if needed
